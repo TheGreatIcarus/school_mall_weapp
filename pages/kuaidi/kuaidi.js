52253f158a0284893e00d0cda5208e26b3ebab8f
soul2046 <577579265@qq.com>
2020-09-27 18:56:23 +0800

    delivery first

--- pages/kuaidi/kuaidi.js
+++ pages/kuaidi/kuaidi.js
@@ -12,7 +12,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    
   },
 
   /**
@@ -29,38 +29,23 @@ Page({
   
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  formSubmit(e) {
+    console.log('form发生了submit事件，携带数据为：', e.detail.value);
+    var delivery = JSON.stringify(e.detail.value);
+    wx.request({
+      url: 'http://localhost:8080/Delivery/insertDelivery',
+      data: {
+       delivery
+      },
+      header: {'content-type':'application/x-www-form-urlencoded'},
+      method: 'POST',
+      success: (result)=>{
+        
+      },
+      fail: ()=>{},
+      complete: ()=>{}
+    });
   }
+
+
 })
